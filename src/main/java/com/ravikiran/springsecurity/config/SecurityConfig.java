f24c51953bbf79c684306c1ae1f3983e18cde36c
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-06-15 15:08:52 +0530

    Added ROLE based authorization to the ADMIN and USER roles on methods

--- src/main/java/com/ravikiran/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/ravikiran/springsecurity/config/SecurityConfig.java
@@ -1,9 +1,51 @@
 package com.ravikiran.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin = User.withUsername("ravikiran")
+                                .password(encoder.encode("pwd1"))
+                                .roles("ADMIN")
+                                .build();
+
+        UserDetails simpleUser = User.withUsername("john")
+                                    .password(encoder.encode("pwd2"))
+                                    .roles("USER")
+                                    .build();
+
+        return new InMemoryUserDetailsManager(admin, simpleUser);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/products/welcome").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/products/**")
+                .authenticated()
+                .and().formLogin()
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/ravikiran/springsecurity/controller/ProductController.java
+++ src/main/java/com/ravikiran/springsecurity/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ravikiran.springsecurity.controller;
 import com.ravikiran.springsecurity.entity.Product;
 import com.ravikiran.springsecurity.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,12 +25,14 @@ public class ProductController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<Product> getAllProducts(){
         System.out.println("getAllProducts");
         return this.service.getAllProducts();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public Product getProductById(@PathVariable  int id){
         return this.service.getProductById(id);
     }
